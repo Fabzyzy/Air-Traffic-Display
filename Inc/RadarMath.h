35e778d76c16296d7f512699d33ce34420a69087
Fabzyzy <130939735+Fabzyzy@users.noreply.github.com>
2026-07-27 16:38:04 -0400

    Revert "Checkpoint on refactor"
    This reverts commit 9c3b4879f144e1f4145e14a64981e24751ba4c2d.

--- Inc/RadarMath.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace RadarMath {
-    float degreesToRadians(float degrees);
-    float radiansToDegrees(float radians);
-    float computeDistanceKm(float latitude1, float longitude1, float latitude2, float longitude2);
-    float computeBearingDeg(float latitude1, float longitude1, float latitude2, float longitude2);
-}
